a806722df892b08b1dc0fa2837942b6e86963028
rahmonovsaydullo <saydullo779@gmail.com>
2025-02-24 15:22:48 +0500

    I corrected solution

--- 2621.js
+++ 2621.js
@@ -1,10 +1,17 @@
 // https://leetcode.com/problems/sleep
 
 
-function showHelloWord(word) {
-    setTimeout(() => {
-        console.log(word);
-    }, 500);
+async function sleep(millis) {
+    await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(millis)
+        }, millis);
+    })
+        .then((resolve) => {
+            console.log(resolve);
+        })
+        .catch((err) => console.log(err)
+        )
 }
-
+sleep(100)
 
