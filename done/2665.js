e903df90dcca9e78ee15b6d0bda5d42502b9432f
rahmonovsaydullo <saydullo779@gmail.com>
2025-02-26 12:41:03 +0500

    I solved 2665 problem in leet code

---
+++ done/2665.js
@@ -0,0 +1,20 @@
+var createCounter = function (init) {
+    let num = init
+    return {
+        increment: () => {
+            return num = num + 1
+        },
+        decrement: () => {
+            return num = num - 1
+        },
+        reset: () => {
+            return  num = init
+        }
+    }
+};
+
+
+const counter = createCounter(5)
+counter.increment()
+counter.decrement()
+counter.reset()
