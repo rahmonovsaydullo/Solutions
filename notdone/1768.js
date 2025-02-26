d108243a00560b605fd61ea458d0b6227c7a2a5e
rahmonovsaydullo <saydullo779@gmail.com>
2025-02-26 12:42:17 +0500

    I solved problems

---
+++ notdone/1768.js
@@ -0,0 +1,16 @@
+mergeAlternately = function (word1, word2) {
+    let res1 = ''
+    for (const element of word1) {
+        return res1 = element
+    }
+    console.log(res1);
+
+    let res2 = ''
+    for (const element of word2) {
+        return res2 = element
+
+    }
+    console.log(res1);
+};
+
+console.log(mergeAlternately('abc', 'klm'));
